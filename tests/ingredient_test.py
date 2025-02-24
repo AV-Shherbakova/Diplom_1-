19c72531e38b615a75105e543b73b5ee960badfc
Alyona Shherbakova <assenish88@gmail.com>
2025-02-24 09:55:53 +0300

    Исправление замечаний 2

--- tests/ingredient_test.py
+++ tests/ingredient_test.py
@@ -13,9 +13,30 @@ class TestIngredient:
             (INGREDIENT_TYPE_SAUCE, "Chili", 120)
         ]
     )
-    @allure.title("Проверка создания ингредиента")
-    def test_create_ingredient(self, ingredient_type, name, price):
+    @allure.title("Проверка создания ингредиента (тип)")
+    def test_create_ingredient_type(self, ingredient_type, name, price):
         ingredient: Ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_type() == ingredient_type
+
+    @pytest.mark.parametrize(
+        "ingredient_type, name, price", [
+            (INGREDIENT_TYPE_SAUCE, "Salsa", 100),
+            (INGREDIENT_TYPE_SAUCE, "Chili", 120)
+        ]
+    )
+    @allure.title("Проверка создания ингредиента (цена)")
+    def test_create_ingredient_price(self, ingredient_type, name, price):
+        ingredient: Ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_price() == price
+
+
+    @pytest.mark.parametrize(
+        "ingredient_type, name, price", [
+            (INGREDIENT_TYPE_SAUCE, "Salsa", 100),
+            (INGREDIENT_TYPE_SAUCE, "Chili", 120)
+        ]
+    )
+    @allure.title("Проверка создания ингредиента (название)")
+    def test_create_ingredient_name(self, ingredient_type, name, price):
+        ingredient: Ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_name() == name
